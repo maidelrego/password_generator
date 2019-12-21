26e36348e510898f3b401a740249afe41256319f
Maydel Rego <maesbroso@gmail.com>
2019-12-21 12:21:09 -0600

    last commit

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-var char = 'ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz';
+// creating my s
+
+var char = '';
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWZYZ';
+var lower = 'abcdefghijklmnopqrstuvwxyz';
 var num = '1234567890';
 var sym = '!@#$%^&*()_+';
 
+
 var lengthEl = document.getElementById("slider");
+var upperBox = document.getElementById("upperC");
+var lowerBox = document.getElementById("lowerC");
 var numBox = document.getElementById("numbers");
 var symBox = document.getElementById("symbols");
 var btnGenerate = document.getElementById("generate");
 var yourPass = document.getElementById("display");
 
-btnGenerate.addEventListener("click",function(e){
+
+btnGenerate.addEventListener("click",function(){
     var characters = char;
+    (upperBox.checked) ? characters +=upper : '';
+    (lowerBox.checked) ? characters +=lower : '';
     (numBox.checked) ? characters += num : '';
     (symBox.checked) ? characters += sym : '';
     yourPass.value = password(slider.value, characters);
 });
-
-
-
 
 function password(l , characters){
     var pwd = '';
